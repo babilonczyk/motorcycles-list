71f7a868d4b9466785ea6d2e1adbf0c112b8dbf3
babilonczyk <piotrzkowski.jan@gmail.com>
2020-11-23 20:46:16 +0100

    Test MotorcycleController new method

--- spec/controllers/motorcycles_spec.rb
+++ spec/controllers/motorcycles_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe MotorcyclesController, type: :controller do
   describe "GET index" do
     subject { get :index  }
 
-    it 'should assign @motorcycles' do
+    it 'should change Motorcycles count from 0 to 1' do
         expect{ subject }.to change(Motorcycle, :count).from(0).to(1)
     end
 
@@ -14,6 +14,21 @@ RSpec.describe MotorcyclesController, type: :controller do
       expect(response).to render_template(:index)
     end
 
+  end
+
+
+  describe "GET new" do
+    subject{ get :new }
+
+    it 'should render the new template' do
+      subject
+      expect(response).to render_template(:new)
+    end
+
+    it 'should assign @motorcycle with new Motorcycle Object' do
+      subject
+      expect( assigns( :motorcycle ) ).to be_a_new( Motorcycle )
+    end
   end
 
 end
