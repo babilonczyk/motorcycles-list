c1fd60a3f3f522fe379b7e3f99bfe6d6ad0aeaf2
babilonczyk <piotrzkowski.jan@gmail.com>
2020-12-07 19:03:48 +0100

    Add function to display title based on request method in MotorcycleForm

--- app/javascript/bundles/Motorcycles/components/MotorcycleForm.jsx
+++ app/javascript/bundles/Motorcycles/components/MotorcycleForm.jsx
@@ -8,20 +8,35 @@ const MotorcycleForm = (props) => {
     const [motorcycle, setMotorcycle] = useState(props.motorcycle);
     const [method, setMethod] = useState(props.method);
 
-    function submitHandle() {
-        if ( method === 'UPDATE' )
-            props.update(event, motorcycle.id);
-        else if ( method === 'CREATE' )
-            props.create(event);
+    function title(method) {
+        let title = null;
+        if ( method === 'UPDATE' ) {
+            return title = (
+                <Fragment>
+                    <h4>Edit motorcycle</h4>
+                </Fragment>
+            );
+        } else if ( method === 'CREATE' ) {
+            return title = (
+                <Fragment>
+                    <h4>Add new motorcycle</h4>
+                </Fragment>
+            );
+        }
+    }
 
+    function submitHandle() {
+        if ( method === 'UPDATE' ) {
+            props.update(event, motorcycle.id);
+        } else if ( method === 'CREATE' ) {
+            props.create(event);
+        }
     }
 
     return (
         <form id={"form"} onSubmit={ submitHandle }>
 
-            <h4>Add new motorcycle</h4>
-
-            <p>Name:</p>
+            {title(method)}
 
             <div className={"form-field"}>
                 <input type='text' name='motorcycle[name]' defaultValue={motorcycle.name}/>
